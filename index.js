1ce1f72c2d355a4c95910679b7b06213a345c2fb
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2023-07-13 03:42:49 +0530

    ...

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ app.post("/api/upload", upload.single("file"), function (req, res) {
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT, () => {
     console.log("Connected!");
 });
